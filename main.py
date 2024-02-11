1e74a7860cc8e11f7a75448a5a550659ed872ebb
hojoon.sung <hjsung94@gmail.com>
2024-02-11 22:34:58 +0900

    Docs: documentation 추가

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+version of Python : 3.12
+
+list of Packages
+- beautifulsoup4 : 4.12.3
+- matplotlib : 3.8.2
+- ntlk : latest (3.8.1)
+- seaborn : 0.13.2
+- selenium : 4.17.2
+- wordcloud : 1.9.3
+
+"""
